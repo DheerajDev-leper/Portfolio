a4d047d8d95ea3a6a82550a66e6eb1f5d3af08d4
Dheeraj <dheerajmalvi2004@gmail.com>
2026-06-26 20:11:41 +0530

    Debugging

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -147,12 +147,12 @@ CSRF_TRUSTED_ORIGINS = [
     "https://portfolio-dccu.onrender.com",
     "https://www.portfolio-dccu.onrender.com",
 ]
-
+EMAIL_TIMEOUT = 10
 
 EMAIL_BACKEND       = config('EMAIL_BACKEND')
 EMAIL_HOST          = config('EMAIL_HOST')
 EMAIL_PORT          = config('EMAIL_PORT', cast=int)
-# EMAIL_USE_SSL       =config('EMAIL_USE_SSL', cast=bool)
+EMAIL_USE_SSL       =config('EMAIL_USE_SSL', cast=bool)
 EMAIL_USE_TLS       = config('EMAIL_USE_TLS', cast=bool)
 EMAIL_HOST_USER     = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
